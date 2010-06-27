cb6000ba8f718ea7cfac86c3f47e06cbc549fc0a
pitr <pitr.vern@gmail.com>
2010-06-27 16:45:48 -0400

    show cookie, for debugging

--- main.rb
+++ main.rb
@@ -1,10 +1,26 @@
 require 'rubygems'
 require 'sinatra'
 
-configure do
-  Rack::Mime::MIME_TYPES['.manifest'] = 'text/cache-manifest'
+configure :production do
+  APP_ID = ENV['app_id']
+  APP_SECRET = ENV['app_secret']
+end
+
+helpers do
+  def get_cookie
+    request.cookies['fbs_' + APP_ID]
+  end
+end
+
+not_found do
+  'oops'
+end
+
+error do
+  'err ' + ENV['sinatra.error'].name
 end
 
 get '/' do
-  haml :index
+  get_cookie
+  #haml :index
 end
