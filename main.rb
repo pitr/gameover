c61e564bfc4e827a30acf1dcfa05f5c7db7d8e68
pitr <pitr.vern@gmail.com>
2010-07-04 01:23:54 -0400

    use bundler

--- main.rb
+++ main.rb
@@ -1,4 +1,11 @@
-require 'rubygems'
+begin
+  require ::File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
 require 'sinatra'
 require 'haml'
 
